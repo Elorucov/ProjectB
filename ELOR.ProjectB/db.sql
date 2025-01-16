80ecc9452888046b489b90ee5d117e11f2effa3b
Elorucov <elorucov@live.ru>
2025-01-16 23:18:20 +0400

    Update db.sql and README.md

--- ELOR.ProjectB/db.sql
+++ ELOR.ProjectB/db.sql
@@ -122,6 +122,33 @@ CREATE PROCEDURE `getComments` (`auth_mid` INT UNSIGNED, `report_id` INT UNSIGNE
     SELECT * FROM report_comments WHERE report_id = report_id;
 END$$
 
+DELIMITER $$
+CREATE PROCEDURE `getSingleReport` (`auth_mid` INT UNSIGNED, `report_id` INT UNSIGNED)
+BEGIN
+    DECLARE report_creator_id INT UNSIGNED;
+    DECLARE report_severity TINYINT UNSIGNED;
+    DECLARE product_id INT UNSIGNED;
+    DECLARE product_owner_id INT UNSIGNED;
+    
+    -- Проверка существования отчёта
+    SELECT r.creator_id, r.severity, r.product_id, p.owner_id
+    INTO report_creator_id, report_severity, product_id, product_owner_id
+    FROM reports r
+    JOIN products p ON r.product_id = p.id
+    WHERE r.id = report_id;
+
+    IF report_creator_id IS NULL THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Not found', MYSQL_ERRNO = 7501;
+    END IF;
+
+    IF report_severity = 5 AND (auth_mid != report_creator_id AND auth_mid != product_owner_id) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Access denied', MYSQL_ERRNO = 7502;
+    END IF;
+
+    -- Приходится во второй раз получать отчёт, ибо нельзя всю строку хранить в переменной, а декларировать дупли столбцов таблицы reports не хочу
+    SELECT * FROM reports WHERE id = report_id;
+END$$
+
 CREATE PROCEDURE `testerror` (`id` INT)   BEGIN
     IF id = 3 THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'This is a test error', MYSQL_ERRNO = 7599;
--- README.md
+++ README.md
@@ -32,8 +32,8 @@ Reports with vulnerabilities are available only to the report author and the pro
 Any member can leave a **comment** to any reports except for vulnerabilities. Only the report author and the product owner can leave comments to such reports. A comment is added every time the status or priority of a report changes, but without text.
 
 ## TODO
-[ ] `offset`, `count` and `rev` parameters for products.get, reports.get and reports.getComment
-[ ] Getting reports count for the one product;
-[ ] Getting reports count created by the one member (and grouped by products);
-[ ] Search reports, products and members.
-[ ] (maybe a simple web app to demonstrate?)
+- [ ] `offset`, `count` and `rev` parameters for products.get, reports.get and reports.getComment
+- [ ] Getting reports count for the one product;
+- [ ] Getting reports count created by the one member (and grouped by products);
+- [ ] Search reports, products and members.
+- [ ] (maybe a simple web app to demonstrate?)
